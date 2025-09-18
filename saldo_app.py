1ddc69997d227ace424fcac6bd49bf77543dd256
Fabrizio <fabriziodisciorio91@gmail.com>
2025-09-18 10:36:36 +0200

    Update saldo_app.py

--- saldo_app.py
+++ saldo_app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import plotly.graph_objects as go
 import numpy as np   
 
-from utils.portfolio_utils import download_data, calculate_returns, portfolio_metrics, simulate_investment, simulate_t_copula, create_pdf_report
+from utils.portfolio_utils import download_data, calculate_returns, portfolio_metrics, simulate_investment, simulate_t_copula, create_pdf_report_investimento
 from utils.portfolio_utils import plot_cumulative_returns, plot_return_distribution, plot_weights, plot_drawdown, plot_rolling_volatility, plot_correlation_heatmap,plot_risk_contribution, plot_contribution, plot_efficient_frontier
 
 from fpdf import FPDF
@@ -328,8 +328,7 @@ if 'df_pct' in st.session_state and 'metrics' in st.session_state:
     if st.button("Scarica PDF report"):
         percentili_data = st.session_state['df_pct']
         metrics = st.session_state['metrics']
-        pdf_bytes = create_pdf_report(
-            df, 
+        pdf_bytes = create_pdf_report_investimento(
             st.session_state.get("saldo_annuale", 0),
             metrics=metrics,
             percentili=percentili_data
@@ -342,6 +341,7 @@ if 'df_pct' in st.session_state and 'metrics' in st.session_state:
         )
 else:
     st.info("🔹 Completa prima la simulazione per abilitare il download del PDF.")
+
 
 
 
